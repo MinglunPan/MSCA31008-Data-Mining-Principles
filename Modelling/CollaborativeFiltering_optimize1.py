dd2936f04aabe1c2a346375f0e756a8e34c354a4
AdrianPan <adrianpml_1997@163.com>
2022-03-10 13:53:08 -0600

    Enter

--- Modelling/CollaborativeFiltering_optimize1.py
+++ Modelling/CollaborativeFiltering_optimize1.py
@@ -6,11 +6,11 @@ from scipy.sparse import rand
 from sklearn.preprocessing import normalize
 from sklearn.metrics.pairwise import cosine_similarity
 
-def recommendItemCF_o(user_data, item_similarity_matrix, item_columns):
-    user_records = np.nan_to_num(np.sign(user_data[item_columns]),0).T
+def recommendItemCF_o2(user_data, item_similarity_matrix, item_columns):
+    user_records = np.nan_to_num(np.sign(user_data[item_columns]),0)
     user_preference = np.nan_to_num(
         np.divide(user_data[item_columns].values, user_data[item_columns].sum(axis = 1).values.reshape(-1,1))
-        ,0).T
+        ,0)
     user_results_dict = {}
     for i,idx in tqdm(enumerate(user_data.index)):
         item_list = user_records[i]
@@ -22,8 +22,17 @@ def recommendItemCF_o(user_data, item_similarity_matrix, item_columns):
             )
         )
 
-    return pd.DataFrame(user_results_dict)
+    return pd.DataFrame(user_results_dict).T
 
 
-def item_similarity_o(matrix):
+def item_similarity_o2(matrix):
     return cosine_similarity(matrix)
+
+if __name__ == '__main__':
+    user_data = np.random.random_integers(0, 5, (10,5))
+    user_data = pd.DataFrame(user_data)
+
+    # What we expect here is the similarities of items,
+    # thus we transpose the user matrix
+    sim_mat = item_similarity_o2(user_data.T) 
+    recommendItemCF_o2(user_data, sim_mat, user_data.columns)
