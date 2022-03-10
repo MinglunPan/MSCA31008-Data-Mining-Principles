273bc7ec6b8a8acc55cd7371ae94a5b72b036f3e
Milan Toolsidas <milantoolsidas@Milan-Toolsidass-MacBook-Pro.local>
2022-03-09 19:08:52 -0600

    updated for non square matrices?

--- Modelling/CollaborativeFiltering_optimize_final.py
+++ Modelling/CollaborativeFiltering_optimize_final.py
@@ -8,5 +8,7 @@ def recommendItemCF(user_data, item_columns):
     user_preference = np.divide(user_data[item_columns], 
                                 user_data[item_columns].sum(axis = 1).to_frame()).fillna(0).values
     
-    #return pd.DataFrame(np.einsum("ij,jk,ij->ji", user_records, cosine_similarity(user_data), user_preference))
-    return pd.DataFrame(np.einsum("ij,jk,ij->ji", user_records, cosine_similarity(user_data), user_preference))
+    if user_data.shape[0] == user_data.shape[1]:
+        return pd.DataFrame(np.einsum("ij,jk,ij->ji", user_records, cosine_similarity(user_data), user_preference))
+    else:
+        return pd.DataFrame(np.einsum("ij,jk,ij->ji", user_records.T, cosine_similarity(user_data).T, user_preference.T))
