a9bf11a833eb662885e76c3ddcfe8b8068cc1c0d
MinglunPan <78308558+MinglunPan@users.noreply.github.com>
2022-03-10 01:15:51 -0600

    Update CollaborativeFiltering_optimize1.py

--- Modelling/CollaborativeFiltering_optimize1.py
+++ Modelling/CollaborativeFiltering_optimize1.py
@@ -7,14 +7,20 @@ from sklearn.preprocessing import normalize
 from sklearn.metrics.pairwise import cosine_similarity
 
 def recommendItemCF_o(user_data, item_similarity_matrix, item_columns):
-    user_records = np.sign(user_data[item_columns])
-    user_preference = np.divide(user_data[item_columns], 
-                                user_data[item_columns].sum(axis = 1).to_frame()).fillna(0).values #this line gives issues if dimensions are off -- can transpose?
+    user_records = np.nan_to_num(np.sign(user_data[item_columns]),0).T
+    user_preference = np.nan_to_num(
+        np.divide(user_data[item_columns].values, user_data[item_columns].sum(axis = 1).values.reshape(-1,1))
+        ,0).T
     user_results_dict = {}
     for i,idx in tqdm(enumerate(user_data.index)):
         item_list = user_records[i]
         item_weights = user_preference[i]
-        user_results_dict[idx] = dict(enumerate(np.sum(item_list.values * item_similarity_matrix * item_weights, axis = 0).flatten(), 1)) #this line gives issues if dimensions are off -- can reshape? 
+        user_results_dict[idx] = dict(
+            enumerate(
+                np.sum(item_list * item_similarity_matrix * item_weights, axis = 1),
+                1
+            )
+        )
 
     return pd.DataFrame(user_results_dict)
 
