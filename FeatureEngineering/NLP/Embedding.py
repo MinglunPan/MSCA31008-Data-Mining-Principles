1238c076d0b2cd3f2b8c7f05867b1023e0571ebc
Milan Toolsidas <milantoolsidas@Milan-Toolsidass-MacBook-Pro.local>
2022-03-14 09:38:59 -0500

    comments

--- FeatureEngineering/NLP/Embedding.py
+++ FeatureEngineering/NLP/Embedding.py
@@ -9,43 +9,48 @@ from tqdm.notebook import tqdm
 class Transformer:
     # Abstract class for transformer
     model = None
+    # Fit function
     def fit(self, text_series):
         raise NotImplementedError
+    # Transform function
     def transform(self, text_series):
         raise NotImplementedError
+    # Fit/transform function
     def fit_transform(self, text_series):
         self.fit(text_series)
         return self.transform(text_series)
 
+# TFIDF transformer class
 class TFIDF_Transformer(Transformer):
-    def __init__(self):
+    def __init__(self): # Initialize model with default parameters
         self.model = TfidfVectorizer(max_features = 15000, min_df= 3, max_df=0.5, analyzer = 'char_wb', ngram_range = (3,5))
-    def fit(self, text_series):
+    def fit(self, text_series): # Fit TFIDF
         self.model = self.model.fit(text_series)
-    def transform(self, text_series):
+    def transform(self, text_series): # Transform TFIDF
         return self.model.transform(text_series)
 
+# BERT transformer class
 class BERT_Transformer(Transformer):
     def __init__(self):
-        self.model = BERT_transformer()
-    def fit(self, text_series):
+        self.model = BERT_transformer() # Initialize model
+    def fit(self, text_series): # Fit BERT
         pass
-    def transform(self, text_series):
+    def transform(self, text_series): # Transform BERT
         return self.model(text_series)
 
-
+# BERT transformer function
 def BERT_transformer(tfhub_handle_encoder = TFHUB_HANDLE_ENCODER,
                     tfhub_handle_preprocess = TFHUB_HANDLE_PREPROCESS):
-    text_input = tf.keras.layers.Input(shape=(), dtype=tf.string, name='text')
-    preprocessing_layer = hub.KerasLayer(tfhub_handle_preprocess, name='preprocessing')
-    encoder_inputs = preprocessing_layer(text_input)
-    encoder = hub.KerasLayer(tfhub_handle_encoder, name='BERT_encoder')
-    outputs = encoder(encoder_inputs)['pooled_output']
-    return tf.keras.Model(text_input, outputs)
+    text_input = tf.keras.layers.Input(shape=(), dtype=tf.string, name='text') # Create input layer for text
+    preprocessing_layer = hub.KerasLayer(tfhub_handle_preprocess, name='preprocessing') # Create preprocesing layer
+    encoder_inputs = preprocessing_layer(text_input) # Get encoder inputs by passing text through preprocessing layer
+    encoder = hub.KerasLayer(tfhub_handle_encoder, name='BERT_encoder') # create encoder layer
+    outputs = encoder(encoder_inputs)['pooled_output'] # get outputs by encoding processed texts
+    return tf.keras.Model(text_input, outputs) # return Keras model
 
 
 
-
+# Embedding class
 class Embedding:
     # Administrator to mnanage different transformers and 
     # provide a integrated class to manipulate the transformers
@@ -79,6 +84,7 @@ class Embedding:
         assert method in self.__fit_dict
         return self.__fit_dict.get(method)
     def transform(self, method, text_series, batch_size = None):
+        # transform data
         self.fit_model(method)
         if batch_size == None:
             return self.__transform(method, text_series)
@@ -86,6 +92,7 @@ class Embedding:
             assert isinstance(batch_size, int)
             return self.__batch_transform(method, text_series, batch_size)
     def __batch_transform(self, method, text_series, batch_size):
+        # transform data in batches
         result_list = []
         for i in tqdm(range(0, len(text_series)//batch_size+1)):
             batch_text = text_series.iloc[i*batch_size:(i+1)*batch_size]
