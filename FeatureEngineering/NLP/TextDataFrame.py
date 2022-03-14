fcbd199534927a3139221c73835d5bc131662c4d
Milan Toolsidas <milantoolsidas@Milan-Toolsidass-MacBook-Pro.local>
2022-03-14 09:47:24 -0500

    comments

--- FeatureEngineering/NLP/TextDataFrame.py
+++ FeatureEngineering/NLP/TextDataFrame.py
@@ -8,19 +8,20 @@ from Config import DEFAULT_CLEAN_PROCEDURE
 from tqdm.notebook import tqdm_notebook as tqdm
 tqdm.pandas()
 
+# Create DatasetProcessor class
 class DatasetProcessor:
-    def __init__(self, procedures):
+    def __init__(self, procedures): # initialize procedures
         self.procedures = procedures
     def set_target(self, data, response_col = None, average_weights_dict = None, y_col = 'y'):
         assert (response_col == None)^(average_weights_dict == None)
-        if average_weights_dict != None: 
+        if average_weights_dict != None: # if some average weights in dictionary, return weighted sum of data based on weights
             assert isinstance(average_weights_dict, dict)
             return weightedSum(data, average_weights_dict)
-        if response_col != None: 
+        if response_col != None: # if response column is not None, return the corresponding column from data
             assert isinstance(response_col, str)
             assert response_col in data.columns
             return data[response_col]
-    def clean_text(self, text_series, show_progress = True):
+    def clean_text(self, text_series, show_progress = True): # Initialize text cleaner with given procedures and clean texts
         assert text_series.dtype == 'O'
         cleaner = TextCleaner(self.procedures)
         if show_progress:
--- FeatureEngineering/NLP/utils.py
+++ FeatureEngineering/NLP/utils.py
@@ -6,37 +6,37 @@ import os
 import warnings
 
 
-
+# Function for calculating the weighted sum of data based on a dictionary of weights
 def weightedSum(data, weight_dict):
     return (data[weight_dict.keys()] * np.array(weight_dict)).sum(axis = 1)
 
-
+# Function for under/oversampling
 def sample_binary(data, target_col, 
                   filter_func = lambda x:x == 0, sample_method = 'undersample',
                   random_state = 42
                  ):
-    filter_ = data[target_col].apply(filter_func)
-    filter_count = filter_.value_counts().sort_values()
-    if len(filter_count) != 2:
+    filter_ = data[target_col].apply(filter_func) # Filter data for given target column and use supplied filter function/criteria 
+    filter_count = filter_.value_counts().sort_values() # Get sorted value counts
+    if len(filter_count) != 2: # Warning if filter counts not equal to 2
         warnings.warn("The number of class is not equal to 2")
     # undersample 大的抽小的
     # Oversample 小的抽大的（有放回）
-    min_count, max_count = filter_count.values
-    min_class, max_class = filter_count.index
+    min_count, max_count = filter_count.values # get min and max counts
+    min_class, max_class = filter_count.index # get corresponding min and max classes
     if sample_method == 'undersample':
-        sample_count, sample_class = min_count,max_class
+        sample_count, sample_class = min_count,max_class # if undersampling, use min count (minority class) and set sample class as majority class -- want to reduce max class to min count samples
         replace = False
-    elif sample_method == 'oversample':
+    elif sample_method == 'oversample': # if oversampling, use max count (majority class) and set sample class as minority class -- want to increase min class to max count samples
         sample_count, sample_class = max_count,min_class
         replace = True
-    else:
+    else: # if no sample method is provided...
         raise ValueError("No sample_method: %s"%sample_method)
     filter_ = filter_ == sample_class
-    sample_df = data.loc[filter_].sample(n = sample_count, random_state = random_state, replace = replace)
-    df = pd.concat([sample_df, data.loc[-filter_]], axis = 0)
+    sample_df = data.loc[filter_].sample(n = sample_count, random_state = random_state, replace = replace) # undersample or oversample as determined above/from parameters
+    df = pd.concat([sample_df, data.loc[-filter_]], axis = 0) # concatenate new sample df with filtered df
     return df
 
-
+# Function for setting random seed
 def set_seed(seed = 42):
     '''Sets the seed of the entire notebook so results are the same every time we run.
     This is for REPRODUCIBILITY.'''
